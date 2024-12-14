bb965a3758a9d722834da9f12cb31015dfbbac2b
muhammadubaid786 <muhammadubaid007788@gmail.com>
2024-12-14 10:15:23 +0500

    header

--- air/urls.py
+++ air/urls.py
@@ -7,10 +7,12 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.views.static import serve
+from . import views
 
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
+    path('', views.home , name='home')
 ]
 
